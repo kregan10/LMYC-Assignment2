12692a9413268018654172b40b797571217487d0
Kerry Regan <kregan89@gmail.com>
2018-04-05 10:35:45 -0700

    restructured project

---
+++ LmycWeb/Data/Migrations/20180405171750_First Migration.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LmycWeb.Data.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /*migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+                */
+            migrationBuilder.CreateTable(
+                name: "Boats",
+                columns: table => new
+                {
+                    BoatId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BoatName = table.Column<string>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    LengthInFeet = table.Column<int>(nullable: false),
+                    Make = table.Column<string>(nullable: false),
+                    Picture = table.Column<string>(nullable: false),
+                    Record = table.Column<string>(nullable: false),
+                    Year = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boats", x => x.BoatId);
+                    table.ForeignKey(
+                        name: "FK_Boats_AspNetUsers_CreatedBy",
+                        column: x => x.CreatedBy,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+/*
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+                */
+            migrationBuilder.CreateIndex(
+                name: "IX_Boats_CreatedBy",
+                table: "Boats",
+                column: "CreatedBy");
+            /*
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+                */
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {/*
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+                */
+            migrationBuilder.DropTable(
+                name: "Boats");
+            /*
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+                */
+        }
+    }
+}
---
+++ LmycWeb/Models/Boat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LmycWeb.Models
+{
+    public class Boat
+    {
+
+        [Key]
+        public int BoatId { get; set; }
+
+        [Required(ErrorMessage = "A name is required")]
+        [Display(Name = "Name")]
+        public string BoatName { get; set; }
+
+        [Required(ErrorMessage = "A picture url is required")]
+        public string Picture { get; set; }
+        [Required(ErrorMessage = "Length is required")]
+        [Display(Name = "Length In Feet")]
+        public int LengthInFeet { get; set; }
+
+        [Required(ErrorMessage = "A make is required")]
+        public string Make { get; set; }
+
+        [Required(ErrorMessage = "A year is required")]
+        public string Year { get; set; }
+
+        [Required(ErrorMessage = "A record is required")]
+        public string Record { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Register Date")]
+        public DateTime CreationDate { get; set; }
+
+        [ForeignKey("User")]
+        [ScaffoldColumn(false)]
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+
+        [ScaffoldColumn(false)]
+        public ApplicationUser User { get; set; }
+    }
+}
